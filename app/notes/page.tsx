4a54441c4f711bc39fd6c081b6361a86edea1789
Elikem Kuivi <eykuivi@gmail.com>
2025-03-13 20:58:32 -0700

    Fix error connecting to supabase

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,8 +2,8 @@ import { createClient } from "../../utils/supabase/server";
 
 export default async function Notes() {
     const supabase = await createClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     );
     const { data: notes } = await supabase.from("notes").select();
 
